9433a8bc184931059f26ec408e9a048e62770523
AudreyFelicio <audreyanwar@gmail.com>
2020-05-17 15:15:14 +0800

    add linter dependencies

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+    'prettier',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 11,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+    'prettier',
+  ],
+  rules: {
+    'react/jsx-filename-extension': 'off',
+    'prettier/prettier': 'error',
+  },
+};
