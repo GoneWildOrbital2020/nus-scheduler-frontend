38ad8cb12484bbf3813a5d4d413ce1c7c4c9b126
AudreyFelicio <audreyanwar@gmail.com>
2020-05-19 15:05:05 +0800

    update navbar, add login and source

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Navbar from './components/navbar';
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Switch>
+          <Route path="/login" />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -5,23 +5,55 @@ import {
   Typography,
   Button,
 } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import MenuIcon from '@material-ui/icons/Menu';
 import React from 'react';
+import { Link } from 'react-router-dom';
+import '../css/navbar.css';
+
+const useStyles = makeStyles({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: 0,
+  },
+  title: {
+    color: 'white',
+    flexGrow: 1,
+    textAlign: 'left',
+  },
+  button: {
+    color: 'white',
+  },
+});
 
 const Navbar = () => {
+  const classes = useStyles();
   return (
     <div>
       <AppBar position="static">
         <Toolbar>
-          {/* <IconButton
+          <IconButton
             edge="start"
             className={classes.menuButton}
             color="inherit"
-            aria-label="menu"
-          />
+            aria-label="menu">
+            <MenuIcon />
+          </IconButton>
           <Typography variant="h6" className={classes.title}>
-            News
-          </Typography> */}
-          <Button color="inherit">Login</Button>
+            NUS Scheduler
+          </Typography>
+          <Link to="/login">
+            <Button color="inherit" className={classes.button}>
+              Login
+            </Button>
+          </Link>
+          <Button color="inherit" className={classes.button}>
+            <a href="https://github.com/GoneWildOrbital2020/nus-scheduler-frontend">
+              Source
+            </a>
+          </Button>
         </Toolbar>
       </AppBar>
     </div>
