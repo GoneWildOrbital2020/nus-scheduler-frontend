c74b07fc67291748d535818f6575e4270807ce10
AudreyFelicio <audreyanwar@gmail.com>
2020-05-24 01:59:25 +0800

    add states to login

--- src/components/login.js
+++ src/components/login.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { TextField } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import '../css/login.css';
+import logo from '../images/logo.png';
 
 const useStyles = makeStyles({
   root: {
@@ -12,11 +13,37 @@ const useStyles = makeStyles({
 
 const Login = () => {
   const classes = useStyles();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const handleChangeEmail = (event) => {
+    console.log(event.target.value);
+    setEmail(event.target.value);
+  };
+  const handleChangePassword = (event) => {
+    console.log(event.target.value);
+    setPassword(event.target.value);
+  };
   return (
     <div className="login">
       <div className="form">
+        <img src={logo} alt="logo" />
         <form className={classes.root}>
-          <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+          <div className="input">
+            <TextField
+              label="email"
+              variant="outlined"
+              value={email}
+              onChange={handleChangeEmail}
+            />
+          </div>
+          <div className="input">
+            <TextField
+              label="password"
+              variant="outlined"
+              value={password}
+              onChange={handleChangePassword}
+            />
+          </div>
         </form>
       </div>
       <h1>Login page!</h1>
