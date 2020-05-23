cf143588d87ecbd7cf6ec9ea92774f286b0964d1
AudreyFelicio <audreyanwar@gmail.com>
2020-05-24 00:17:24 +0800

    add form for login page

--- src/components/login.js
+++ src/components/login.js
@@ -1,10 +1,24 @@
 import React from 'react';
-// import { TextField } from '@material-ui/core';
-// import { MakeStyles } from '@material-ui/core';
+import { TextField } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import '../css/login.css';
+
+const useStyles = makeStyles({
+  root: {
+    width: '100%',
+    position: 'relative',
+  },
+});
 
 const Login = () => {
+  const classes = useStyles();
   return (
-    <div>
+    <div className="login">
+      <div className="form">
+        <form className={classes.root}>
+          <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+        </form>
+      </div>
       <h1>Login page!</h1>
     </div>
   );
