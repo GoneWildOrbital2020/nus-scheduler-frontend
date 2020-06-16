7cce971dcaabe79844b8b1b761577bfcc203d44b
AudreyFelicio <audreyanwar@gmail.com>
2020-06-16 20:24:19 +0800

    create initial table and fetch files

--- src/components/upload.js
+++ src/components/upload.js
@@ -1,19 +1,93 @@
-import React, { useState } from 'react';
+/* eslint-disable react/jsx-props-no-spreading */
+import React, { useState, forwardRef, useEffect } from 'react';
 import { makeStyles, Button, TextField } from '@material-ui/core';
+import MaterialTable from 'material-table';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import AddBox from '@material-ui/icons/AddBox';
+import ArrowDownward from '@material-ui/icons/ArrowDownward';
+import Check from '@material-ui/icons/Check';
+import ChevronLeft from '@material-ui/icons/ChevronLeft';
+import ChevronRight from '@material-ui/icons/ChevronRight';
+import Clear from '@material-ui/icons/Clear';
+import DeleteOutline from '@material-ui/icons/DeleteOutline';
+import Edit from '@material-ui/icons/Edit';
+import FilterList from '@material-ui/icons/FilterList';
+import FirstPage from '@material-ui/icons/FirstPage';
+import LastPage from '@material-ui/icons/LastPage';
+import Remove from '@material-ui/icons/Remove';
+import SaveAlt from '@material-ui/icons/SaveAlt';
+import Search from '@material-ui/icons/Search';
+import ViewColumn from '@material-ui/icons/ViewColumn';
+
+const tableIcons = {
+  Add: forwardRef((props, ref) => <AddBox {...props} ref={ref} />),
+  Check: forwardRef((props, ref) => <Check {...props} ref={ref} />),
+  Clear: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
+  Delete: forwardRef((props, ref) => <DeleteOutline {...props} ref={ref} />),
+  DetailPanel: forwardRef((props, ref) => (
+    <ChevronRight {...props} ref={ref} />
+  )),
+  Edit: forwardRef((props, ref) => <Edit {...props} ref={ref} />),
+  Export: forwardRef((props, ref) => <SaveAlt {...props} ref={ref} />),
+  Filter: forwardRef((props, ref) => <FilterList {...props} ref={ref} />),
+  FirstPage: forwardRef((props, ref) => <FirstPage {...props} ref={ref} />),
+  LastPage: forwardRef((props, ref) => <LastPage {...props} ref={ref} />),
+  NextPage: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
+  PreviousPage: forwardRef((props, ref) => (
+    <ChevronLeft {...props} ref={ref} />
+  )),
+  ResetSearch: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
+  Search: forwardRef((props, ref) => <Search {...props} ref={ref} />),
+  SortArrow: forwardRef((props, ref) => <ArrowDownward {...props} ref={ref} />),
+  ThirdStateCheck: forwardRef((props, ref) => <Remove {...props} ref={ref} />),
+  ViewColumn: forwardRef((props, ref) => <ViewColumn {...props} ref={ref} />),
+};
 
 const useStyles = makeStyles(() => ({
-  root: {},
+  root: {
+    width: '75%',
+    margin: '1em auto',
+  },
   input: {
     display: 'none',
   },
 }));
 
 const Upload = (props) => {
-  const { token } = props;
+  const { token, username } = props;
   const classes = useStyles();
   const [name, setName] = useState('');
+  const [file, setFile] = useState([]);
+  const [image, setImage] = useState([]);
+  useEffect(() => {
+    fetch(`http://localhost:8000/calendars/get/file/${username}/CS1101S`, {
+      method: 'GET',
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    })
+      .then((res) => {
+        return res.json();
+      })
+      .then((json) => {
+        console.log(json);
+        setFile(json);
+      });
+    fetch(`http://localhost:8000/calendars/get/image/${username}/CS1101S`, {
+      method: 'GET',
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    })
+      .then((res) => {
+        return res.json();
+      })
+      .then((json) => {
+        console.log(json);
+        setImage(json);
+      });
+  }, []);
   const handleChangeName = (event) => {
     setName(event.target.value);
   };
@@ -89,17 +163,32 @@ const Upload = (props) => {
         value={name}
         onChange={handleChangeName}
       />
+      <MaterialTable
+        icons={tableIcons}
+        title="Insert Module Name Files"
+        columns={[
+          { title: 'Name', field: 'name' },
+          { title: 'Date', field: 'date' },
+          { title: 'File Type', field: 'fileType' },
+        ]}
+        data={[
+          { name: 'testImage', date: '2020/06/14', fileType: '.jpeg' },
+          { name: 'testFile', date: '2020/06/14', fileType: '.pdf' },
+        ]}
+      />
     </div>
   );
 };
 
 Upload.propTypes = {
   token: PropTypes.string.isRequired,
+  username: PropTypes.string.isRequired,
 };
 
 const mapStateToProps = (state) => {
   return {
     token: state.token,
+    username: state.username,
   };
 };
 
