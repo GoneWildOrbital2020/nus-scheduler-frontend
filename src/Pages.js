0926d7baa69e16d19dfb30ac8a32054d24a4d8b3
ZoroarkDarkrai <fidellawi@gmail.com>
2020-07-21 12:02:43 +0800

    improve pages perfomance

--- src/Pages.js
+++ src/Pages.js
@@ -2,20 +2,23 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import { makeStyles, ThemeProvider } from '@material-ui/core';
 import ScopedCssBaseline from '@material-ui/core/ScopedCssBaseline';
-import Authenticate from './components/Authenticate';
-import AutomaticLogout from './components/AutomaticLogout';
-import ForgotPassword from './components/ForgotPassword';
-import ResetPassword from './components/ResetPassword';
-import EventGroup from './components/EventGroup';
-import Profile from './components/profile';
-import Upload from './components/upload';
-import NotesGrid from './components/notesGrid';
-import Login from './components/login';
-import Signup from './components/signup';
-import Calendar from './components/Calendar';
-import Footer from './components/footer';
-import NavbarDrawer from './components/NavbarDrawer';
 import { theme } from './theme';
+
+const Authenticate = React.lazy(() => import('./components/Authenticate'));
+const AutomaticLogout = React.lazy(() =>
+  import('./components/AutomaticLogout'),
+);
+const ForgotPassword = React.lazy(() => import('./components/ForgotPassword'));
+const ResetPassword = React.lazy(() => import('./components/ResetPassword'));
+const EventGroup = React.lazy(() => import('./components/EventGroup'));
+const Profile = React.lazy(() => import('./components/profile'));
+const Upload = React.lazy(() => import('./components/upload'));
+const NotesGrid = React.lazy(() => import('./components/notesGrid'));
+const Login = React.lazy(() => import('./components/login'));
+const Signup = React.lazy(() => import('./components/signup'));
+const Calendar = React.lazy(() => import('./components/Calendar'));
+const Footer = React.lazy(() => import('./components/footer'));
+const NavbarDrawer = React.lazy(() => import('./components/NavbarDrawer'));
 
 const useStyles = makeStyles(() => ({
   app: {
@@ -35,37 +38,43 @@ const Pages = () => {
           <NavbarDrawer elevation={4} />
           <div className="content" style={{ marginTop: '75px' }}>
             <Switch>
-              <Route path="/calendar" exact component={Calendar} />
-              <Route
-                path="/activate/:email/:token"
-                render={({ match }) => (
-                  <Authenticate
-                    email={match.params.email}
-                    token={match.params.token}
-                  />
-                )}
-              />
-              <Route
-                path="/reset/:email/:token"
-                render={({ match }) => (
-                  <ResetPassword
-                    email={match.params.email}
-                    token={match.params.token}
-                  />
-                )}
-              />
-              <Route path="/login" exact component={Login} />
-              <Route path="/signup" exact component={Signup} />
-              <Route
-                path="/events-group/:name"
-                render={({ match }) => (
-                  <EventGroup name={match.params.name} path={match.path} />
-                )}
-              />
-              <Route path="/upload" exact component={Upload} />
-              <Route path="/upload/notes" exact component={NotesGrid} />
-              <Route path="/profile" exact render={() => <Profile />} />
-              <Route path="/forgotPassword" exact component={ForgotPassword} />
+              <React.Suspense fallback={null}>
+                <Route path="/calendar" exact component={Calendar} />
+                <Route
+                  path="/activate/:email/:token"
+                  render={({ match }) => (
+                    <Authenticate
+                      email={match.params.email}
+                      token={match.params.token}
+                    />
+                  )}
+                />
+                <Route
+                  path="/reset/:email/:token"
+                  render={({ match }) => (
+                    <ResetPassword
+                      email={match.params.email}
+                      token={match.params.token}
+                    />
+                  )}
+                />
+                <Route path="/login" exact component={Login} />
+                <Route path="/signup" exact component={Signup} />
+                <Route
+                  path="/events-group/:name"
+                  render={({ match }) => (
+                    <EventGroup name={match.params.name} path={match.path} />
+                  )}
+                />
+                <Route path="/upload" exact component={Upload} />
+                <Route path="/upload/notes" exact component={NotesGrid} />
+                <Route path="/profile" exact render={() => <Profile />} />
+                <Route
+                  path="/forgotPassword"
+                  exact
+                  component={ForgotPassword}
+                />
+              </React.Suspense>
             </Switch>
           </div>
           <Footer />
